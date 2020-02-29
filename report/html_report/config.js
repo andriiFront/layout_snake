11919f0156d635b009133fb4f505ea493df74ec6
Andrii Badekha <badekha@gmail.com>
2020-02-29 23:25:29 +0200

    deploy static content

---
+++ report/html_report/config.js
@@ -0,0 +1,140 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_1200.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_1200.png",
+        "selector": "document",
+        "fileName": "Entire_document_1200.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "1200",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found C:\\projects\\layout_snake\\backstop_data\\bitmaps_reference\\Entire_document_1200.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_1050.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_1050.png",
+        "selector": "document",
+        "fileName": "Entire_document_1050.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "1050",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found C:\\projects\\layout_snake\\backstop_data\\bitmaps_reference\\Entire_document_1050.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_900.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_900.png",
+        "selector": "document",
+        "fileName": "Entire_document_900.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "900",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -668,
+            "height": -420
+          },
+          "misMatchPercentage": "7.73",
+          "analysisTime": 53
+        },
+        "diffImage": "..\\bitmaps_test\\20200229-232219\\failed_diff_Entire_document_900.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_750.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_750.png",
+        "selector": "document",
+        "fileName": "Entire_document_750.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "750",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found C:\\projects\\layout_snake\\backstop_data\\bitmaps_reference\\Entire_document_750.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_600.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_600.png",
+        "selector": "document",
+        "fileName": "Entire_document_600.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "600",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found C:\\projects\\layout_snake\\backstop_data\\bitmaps_reference\\Entire_document_600.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_450.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_450.png",
+        "selector": "document",
+        "fileName": "Entire_document_450.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "450",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found C:\\projects\\layout_snake\\backstop_data\\bitmaps_reference\\Entire_document_450.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_300.png",
+        "test": "..\\bitmaps_test\\20200229-232219\\Entire_document_300.png",
+        "selector": "document",
+        "fileName": "Entire_document_300.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/snake/",
+        "expect": 0,
+        "viewportLabel": "300",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found C:\\projects\\layout_snake\\backstop_data\\bitmaps_reference\\Entire_document_300.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
